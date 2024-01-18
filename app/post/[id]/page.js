2fb5929b2d813a9c964ff84629cda5bc1a0410a1
imran-hossain <engg.imran22@gmail.com>
2024-01-18 15:20:05 +0600

    post comment added

--- app/post/[id]/page.js
+++ app/post/[id]/page.js
@@ -1,5 +1,7 @@
+import Comments from '@/app/Comments';
+import allComments from '@/lib/getComments';
 import getSinglePost from '@/lib/getSinglePost';
-import React from 'react';
+import { Suspense } from 'react';
 export async function generateMetadata({params}){
     const {id} = params
     const post = await getSinglePost(id)
@@ -10,15 +12,21 @@ export async function generateMetadata({params}){
 }
 const singlePost = async ({params}) => {
     const {id} =params
-    const  post = await getSinglePost(id)
+    const  postPromise =  getSinglePost(id)
+    const commentPorimse = allComments(id)
+    const post = await  postPromise
+
     return (
         <div>
             
-            <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
 
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{post.title}</h5>
-            <p class="font-normal text-gray-700 dark:text-gray-400">{post.body}</p>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{post.title}</h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">{post.body}</p>
             </a>
+            <Suspense fallback='<h1>Loading comment..........</h1>'>
+              <Comments promise={commentPorimse}></Comments>
+            </Suspense>
 
             
         </div>
